b43214f1f61c882b36ee4e8fd134bebfe1719a16
Di3bold <erwandiebold54@gmail.com>
2023-04-25 11:39:43 +0200

    Creation of a Variable Manager

--- variable/RealVariable.py
+++ variable/RealVariable.py
@@ -4,11 +4,12 @@ Created on Thu Apr 20 11:03:01 2023
 @author: questiero
 """
 from Variable import Variable
+from VariableManager import VariableManager
 class RealVariable(Variable):
     def __init__(self, name):
         self._name = name
 
     def declare(name):
         new = RealVariable(name)
-        Variable.add(new)
+        VariableManager.add(new)
         return new
--- variable/Variable.py
+++ variable/Variable.py
@@ -1,29 +1,12 @@
-from abc import ABC
 """
 Created on Thu Apr 20 11:03:01 2023
 
 @author: di3bold
 """
-
+from abc import ABC
 class Variable(ABC):
     instance = {}
     _name = ""
 
     def __init__(self):
         raise Exception("Variable can't have an instance")
-
-    def add(obj):
-        """
-            Method used for adding an instance of a Variable in the instance's dictionary
-            
-            params:
-                obj : Variable
-        """
-        if(isinstance(obj, __class__)):
-            if(obj._name not in __class__.instance):
-                __class__.instance[obj._name] = obj.__class__
-            else:
-                if(not isinstance(obj,  __class__.instance[obj._name])):
-                    raise Exception(f"{obj._name} is already define with another type.")
-        else:
-            raise Exception(f"{obj} is not a Variable.")
