eeb30d40cdd9eff52ab6f93fe28a5f94c2adb278
Di3bold <erwandiebold54@gmail.com>
2023-04-25 11:01:24 +0200

    Adding RealVariable class

--- variable/RealVariable.py
+++ variable/RealVariable.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Thu Apr 20 11:03:01 2023
 
 @author: questiero
 """
+from Variable import Variable
+class RealVariable(Variable):
+    def __init__(self, name):
+        self._name = name
 
+    def declare(name):
+        new = RealVariable(name)
+        Variable.add(new)
+        return new
+    
+test = RealVariable.declare("x")
