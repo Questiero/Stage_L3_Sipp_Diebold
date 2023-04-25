a7c0a80e58d84e9b3880420218b4a5bd8d6aecad
Questiero <19290903+Questiero@users.noreply.github.com>
2023-04-25 15:32:15 +0200

    LinearConstraint

--- src/formula/nullary/constraint/LinearConstraint.py
+++ src/formula/nullary/constraint/LinearConstraint.py
@@ -1,7 +1,24 @@
 from .Constraint import Constraint
 from ....variable import Variable
+from fractions import Fraction
+from enum import Enum
+
+class _ConstraintSymbol(Enum):
+    LEQ: "<="
+    GEQ: ">="
+    EQ: "=="
 
 class LinearConstraint(Constraint):
     
     _symbol = None
     
+    __variables: dict[Variable: Fraction]
+    __sign = _ConstraintSymbol
+    __limit = Fraction
+    
+    def __init__(self, string):
+        #TODO Parser
+        pass
+    
+    def getVariables(self):
+        return self._children.getKeys()
