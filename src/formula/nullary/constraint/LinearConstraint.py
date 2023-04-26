423b4dc3c1bd2a99ad1ac7172be2ef9c7f07cf64
Questiero <19290903+Questiero@users.noreply.github.com>
2023-04-26 11:31:26 +0200

    parser v1 (might need more errors/exceptions)

--- src/formula/nullary/constraint/LinearConstraint.py
+++ src/formula/nullary/constraint/LinearConstraint.py
@@ -1,5 +1,6 @@
 from .Constraint import Constraint
 from ....variable import Variable
+from ....variable import VariableManager
 from fractions import Fraction
 from enum import Enum
 import re
@@ -13,40 +14,42 @@ class LinearConstraint(Constraint):
     
     _symbol = None
     
+    __variables = {}
     __variables: dict[Variable: Fraction]
-    __operator: ConstraintOperator()
+    __operator: ConstraintOperator
     __bound: Fraction
     
     
     """
     rules:
+        - only accepted characters are 1234567890*/+-=<>. and letters
         - spaces doesn't matter
         - >=, == and <= only accepted operators
         - var needs to be defined before
         - var names must start with a letter and be followed by numbers or letters
         - case doesn't matter
         - always a * between var and coef
-        - no need for parenthesis, brackets or whatnot
-        - only accepted characters are 1234567890*/+-=<> and letters
+        - no duplicates variables in the same constraint
         
     exemples:
         - 2/3*x + 3*y - z >= 12/4
-        - 1 / 2 * z = = - 5
+        - 1 / 2 * x + z = = - 5.3
         - -3/4*y-2/3*z<=0
         
     """
     def __init__(self, string):
         
-        unknownChar = re.match("[^\d a-zA-Z/\*+-<>=]")
+        # rule 1, only accepted characters 
+        unknownChar = re.match(r"[^\d a-zA-Z/\*+\-<>=\.]", string)
         if unknownChar:
             raise SyntaxError(f"Unknown character {unknownChar}")
         
-        # rule 1, spaces doesn't matter
+        # rule 2, spaces doesn't matter
         string.replace(" ", "")
         
         leftRightParts = []
         
-        #rule 2, only accepted operators
+        #rule 3, only accepted operators
         if string.find("<="):
             leftRightParts = string.split("<=")
             self.__operator = ConstraintOperator.LEQ
@@ -58,10 +61,38 @@ class LinearConstraint(Constraint):
             self.__operator = ConstraintOperator.EQ
         else:
             raise SyntaxError("Operator not recognized")
-            
-        self.__bound = Fraction(leftRightParts[1])
         
-        leftRightParts[0].replace("-", "-+")
+        if leftRightParts[1] == "":
+            raise SyntaxError("Bound not found")
+        else:
+            self.__bound = Fraction(leftRightParts[1])
+        
+        leftRightParts[0].replace("-", "+-")
+        splitLeft = leftRightParts[0].split("+")
+        
+        for split in splitLeft:
+            
+            var = None
+            coef = None
+            
+            if split.find("*"):
+                (coefString, varName) = split.split("*")
+                var = VariableManager.VariableManager.get(varName.lower())
+                coef = Fraction(coefString)
+            else:
+                if split[0] == "-":
+                    coef = Fraction("-1")
+                    var = VariableManager.VariableManager.get(split[1:].lower())
+                else:
+                    coef = Fraction("1")
+                    var = VariableManager.VariableManager.get(split.lower())
+            
+            if var in self.__variables:
+                raise ValueError(f"Duplicate variable {var._name} found")
+            else:
+                self.__variables.append(var, coef)
+                
+            
     
     def getVariables(self):
         return self._children.getKeys()
