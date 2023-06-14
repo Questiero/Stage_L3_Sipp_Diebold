040c2c18195cad0781310a6bd8367b52c3981af3
Questiero <19290903+Questiero@users.noreply.github.com>
2023-06-14 15:01:43 +0200

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 class Constants():
     
-    ONLY_ONE_SOLUTION = True # False par défaut plus tard
+    ONLY_ONE_SOLUTION = True #TODO False par défaut plus tard
     AND_PARSER_OPERATOR = "&"
     OR_PARSER_OPERATOR = "|"
     NOT_PARSER_OPERATOR = "~"
