d62c4d359a3086c832552dfcf1a5de0b57edb8cc
Questiero <19290903+Questiero@users.noreply.github.com>
2023-06-16 23:16:23 +0200

    Update projector.py

--- src/projector.py
+++ src/projector.py
@@ -58,21 +58,21 @@ class Projector:
         # Fourth step: Get all non parallel combinations
         nonParallelCombinations = list(itertools.combinations(hyperplanes, len(phi.getVariables())))
 
-      #  for hyperplaneCombination in itertools.combinations(hyperplanes, len(phi.getVariables())):
-      #          
-       #     foundParallel = False
-#
-       #     for combinationPair in itertools.combinations(hyperplaneCombination, 2):
-#
-       #         x = combinationPair[0][0]
-       #         y = combinationPair[1][0]
-#
-       #         if (np.dot(x,y)*np.dot(x,y) == np.dot(x,x)*np.dot(y,y)):
-       #             foundParallel = True
-        #            break
-#
-        #    if not foundParallel:
-        #        nonParallelCombinations.append(hyperplaneCombination)
+#       for hyperplaneCombination in itertools.combinations(hyperplanes, len(phi.getVariables())):
+#               
+#           foundParallel = False
+#       
+#           for combinationPair in itertools.combinations(hyperplaneCombination, 2):
+#       
+#               x = combinationPair[0][0]
+#               y = combinationPair[1][0]
+#       
+#               if (np.dot(x,y)*np.dot(x,y) == np.dot(x,x)*np.dot(y,y)):
+#                   foundParallel = True
+#                   break
+#       
+#           if not foundParallel:
+#               nonParallelCombinations.append(hyperplaneCombination)
 
         # Fifth step: Get all vertices from combinations
         vertices = list()
@@ -107,26 +107,39 @@ class Projector:
 
         # Seventh step: Get convex Hull
         hull = ConvexHull(projectedVertices)
-
-        for simplex in hull.simplices:
-            print("-----")
-            print(projectedVertices[simplex, 0])
-            print("---")
-            print(projectedVertices[simplex, 1])
-            print("-----")
-
-        # Get constraints from hull equations
+        
+        # Eighth step: Get constraints from hull simplices
         #TODO know which constraint operator to use
         constraintSet = set()
-        for eq in hull.equations:
+        for simplex in hull.simplices:
+
+            # Get centroid and normal
+            points = projectedVertices[simplex]
+            centroid = np.mean(points, axis=0)
+            u, s, vh = np.linalg.svd(points - centroid, full_matrices=False)
+            normal = vh[-1]
+            
+            # Build constraint
             lc = LinearConstraint("")
-            for i in range(len(variables)):
-                coef = round(Fraction(eq[i]), 12)
-                if coef != 0:
-                    lc.variables[variables[i]] = round(Fraction(eq[i]), 12)
-            lc.operator = ConstraintOperator.EQ
-            lc.bound = round(Fraction(eq[-1]), 12)
+            for i in range(len(normal)):
+                if normal[i] != 0:
+                    lc.variables[variables[i]] = round(Fraction(normal[i]), 12)
+            lc.bound = round(Fraction(np.sum(normal * centroid)), 12)
+
+            for vertex in projectedVertices:
+
+                sum = Fraction("0")
+                for i in range(len(variables)):
+                    coef = lc.variables.get(variables[i])
+                    if coef:
+                        sum += vertex[i]*coef
+                if(sum < lc.bound):
+                    lc.operator = ConstraintOperator.LEQ
+                elif(sum > lc.bound):
+                    lc.operator = ConstraintOperator.GEQ
+                else:
+                    lc.operator = ConstraintOperator.EQ
+
             constraintSet.add(lc)
 
-        print(And(formulaSet = constraintSet))
         return And(formulaSet = constraintSet)
