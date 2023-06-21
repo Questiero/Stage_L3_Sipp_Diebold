e4bb16151d81779f71b841f6d46800664ef6faad
Questiero <19290903+Questiero@users.noreply.github.com>
2023-06-21 13:02:30 +0200

    Create test_cocktail_simplified.py

---
+++ test_cocktail_simplified.py
@@ -0,0 +1,28 @@
+from src.linearConstraint import LinearConstraint
+from src.LPSolverRounded import LPSolverRounded
+from src.revision import Revision
+from src.realVariable import RealVariable
+from src.discreteL1DistanceFunction import discreteL1DistanceFunction
+from src.caron import Caron
+from fractions import Fraction
+
+weights = {
+    RealVariable.declare("vol_tequila"): Fraction(1),
+    RealVariable.declare("vol_sirop"): Fraction(1),
+    RealVariable.declare("vol_alcool"): Fraction(20),
+    RealVariable.declare("pouvoirSucrant"): Fraction(20),
+}
+
+solver = LPSolverRounded()
+simplifier = [Caron(solver)]
+
+cd = LinearConstraint("vol_tequila >= 0") & LinearConstraint("0.35*vol_tequila - vol_alcool = 0")\
+    & LinearConstraint("0.6*vol_sirop + 0.2 * vol_tequila - pouvoirSucrant = 0")
+psi = LinearConstraint("vol_tequila = 4") & LinearConstraint("vol_sirop = 2") & cd
+mu = LinearConstraint("vol_alcool = 0") & cd
+
+rev = Revision(solver, discreteL1DistanceFunction(weights), simplifier, onlyOneSolution=False)
+res = rev.execute(psi, mu)
+
+print("-------")
+print(str(res[0]) + "; " + str(res[1]))
