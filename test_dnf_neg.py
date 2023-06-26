d90d4fb58e117cc11d396a6f5684e1f3f399bfe3
Questiero <19290903+Questiero@users.noreply.github.com>
2023-06-26 12:08:38 +0200

    test_dnf_neg + bugfix + LAISSE MES PRINTS (ou commente-les)

---
+++ test_dnf_neg.py
@@ -0,0 +1,31 @@
+from src.linearConstraint import LinearConstraint
+from src.LPSolverRounded import LPSolverRounded
+from src.revision import Revision
+from src.realVariable import RealVariable
+from src.discreteL1DistanceFunction import discreteL1DistanceFunction
+from src.daalmans import Daalmans
+from src.caron import Caron
+from fractions import Fraction
+
+x = RealVariable.declare("x")
+y = RealVariable.declare("y")
+
+weights = {
+    RealVariable.declare("x"): Fraction(1),
+    RealVariable.declare("y"): Fraction(1),
+}
+
+solver = LPSolverRounded()
+simplifier = [Daalmans(solver)]
+
+psi = LinearConstraint("x >= 0") & LinearConstraint("y >= 0") & LinearConstraint("x + y <= 4")
+mu = (~LinearConstraint("x + y <= 6") & ~LinearConstraint("5*x + y >= 25") & ~LinearConstraint("-0.5*x + y >= 3") & ~LinearConstraint("1/3*x + y <= 4"))\
+    | (~LinearConstraint("5*x + y <= 25") & ~LinearConstraint("3*x + y <= 16") & ~LinearConstraint("-x + y <= -4") & ~LinearConstraint("x >= 6") & ~LinearConstraint("x + y >= 9"))
+      
+rev = Revision(solver, discreteL1DistanceFunction(weights), simplifier, onlyOneSolution=False)
+res = rev.execute(psi, mu)
+
+print("-------")
+print(res[0], ";", res[1])
+
+print("Satisfiable ?", simplifier[0]._interpreter.sat(res[1].toLessOrEqConstraint().toDNF()))
