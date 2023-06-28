5ec1efa275702e05dbc1af2096e77b506ae305ef
Di3bold <erwandiebold54@gmail.com>
2023-06-28 10:50:38 +0200

    Adding a test

---
+++ test_square.py
@@ -0,0 +1,36 @@
+from src.linearConstraint import LinearConstraint
+from src.LPSolverRounded import LPSolverRounded
+from src.revision import Revision
+from src.realVariable import RealVariable
+from src.discreteL1DistanceFunction import discreteL1DistanceFunction
+from src.daalmans import Daalmans
+from src.constants import Constants
+from src.floatConvexHullProjector import FloatConvexHullProjector
+
+from fractions import Fraction
+x = RealVariable.declare("x")
+y = RealVariable.declare("y")
+
+weights = {
+    RealVariable.declare("x"): Fraction(1),
+    RealVariable.declare("y"): Fraction(1),
+}
+
+solver = LPSolverRounded()
+simplifier = [Daalmans(solver)]
+projector = FloatConvexHullProjector(simplification=simplifier, rounding=10)
+mu = LinearConstraint("x >= 2") & LinearConstraint("x <= 5") & LinearConstraint("y >= 2") & LinearConstraint("y <= 5")
+psi = LinearConstraint("x >= 0") & LinearConstraint("x <= 7") & LinearConstraint("y >= 7") & LinearConstraint("y <= 9")\
+| LinearConstraint("x >= 0") & LinearConstraint("x <= 7") & LinearConstraint("y >= -1") & LinearConstraint("y <= 0")\
+| LinearConstraint("x >= 7") & LinearConstraint("x <= 9") & LinearConstraint("y >= 0") & LinearConstraint("y <= 7")\
+| LinearConstraint("x >= -2") & LinearConstraint("x <= 0") & LinearConstraint("y >= 0") & LinearConstraint("y <= 7")
+
+rev = Revision(solver, discreteL1DistanceFunction(weights), simplifier, onlyOneSolution=False, projector=projector)
+res = rev.execute(psi, mu)
+
+print("-------")
+print(str(res[0]) + "; " + str(res[1]))
+
+from src.formulaDisplay import FormulaDisplay
+display = FormulaDisplay()
+display.display({psi.toLessOrEqConstraint() : 'blue', mu.toLessOrEqConstraint() : 'green', res[1].toLessOrEqConstraint() : 'red'}, [x,y])
