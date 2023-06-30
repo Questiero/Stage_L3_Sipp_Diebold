3edb11c8210058bb071f1716c9caa95122df07be
Questiero <19290903+Questiero@users.noreply.github.com>
2023-06-30 10:41:56 +0200

    variable doc

--- src/variable/variable.py
+++ src/variable/variable.py
@@ -1,3 +1,7 @@
+"""
+Abstract class, representing a variable.
+"""
+
 from __future__ import annotations
 
 from fractions import Fraction
@@ -5,21 +9,51 @@ from abc import ABC, abstractmethod
 
 class Variable(ABC):
     '''
-    Abstract Variable class.
-
-    Attributes
-    ----------
-    name: str
-        The name of the Variable
+    Abstract class, representing a variables.
     '''
     
+    #: Name of the variable, by which they are identified.
     name : str = ""
 
     def __init__(self, name):
         self.name = name
 
     @classmethod
-    def declareAnonymous(cls, ending = None) -> Variable:
+    def declareAnonymous(cls, ending: str = None) -> Variable:
+        """
+        Function allowing the user to declar an anonymous variable meant to be used inside algorithms without risking any
+        naming conflit with the standardly defined variables.\n
+        Anonymous variables aren't stored in `src.variable.variableManager.VariableManager.instance` and, as such,
+        can't be used in `src.formula.nullaryFormula.constraint.linearConstraint.LinearConstraint`'s constructor.\n
+
+        Since they can't be used in the usual constructor, one must first declare an empty
+        `src.formula.nullaryFormula.constraint.linearConstraint.LinearConstraint` before adding them manualy.
+
+        To prevent naming conflicts with regular variables, an anonymous variable's name always start with
+        it's object's id. The user can also specify an ending to an anonymous variable name.
+
+        # Usage exemple
+
+        ```py
+            lc = LinearConstraint("")
+
+            for i in range(5):
+                lc.variables[IntegerVariable.declareAnonymous("anonymous")] = 1
+
+            lc.operator = ConstraintOperator.LEQ
+            lc.bound = Fraction("5/2")
+
+            print(lc)
+            >>> 1949361817056anonymous + 1949361816720anonymous + 1949372682384anonymous + 1949435231104anonymous + 1949435231200anonymous <= 5/2
+        ```
+
+        Attributes
+        ----------
+        ending: str
+            The string to concatenate at the end of an anonymous variable's name, after its object id.
+
+        """
+
         v = cls("")
         name = str(id(v))
         if ending:
