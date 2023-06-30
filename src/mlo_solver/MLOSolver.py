1198bebd30fa17828067ebebc11f4468985aa481
Di3bold <erwandiebold54@gmail.com>
2023-06-30 16:17:39 +0200

    Adding documentation on MLOSolver

--- src/mlo_solver/MLOSolver.py
+++ src/mlo_solver/MLOSolver.py
@@ -3,7 +3,27 @@ from __future__ import annotations
 from abc import ABC
 
 class MLOSolver(ABC):
+    '''
+    Abstract MLOSolver class, representing mixed linear constraint solver.
+    '''
+
     def __init__(self):
         raise NotImplementedError("Solver can't have an instance")
     def solve(self, variables : list, objectif : dict, constraints : dict) -> tuple:
+        '''
+        Method returning the result of a mixed linear problem.
+
+        Attributes
+        ----------
+        variables : variables used in constraints
+        objectif : objective function to minimize
+        constraints : list of mixed linear constraints 
+
+        Returns
+        -------
+        optimizationValues : OptimizationValues can be :
+            INFEASIBLE if the problem is infeasible
+            OPTIMAL if the solver have found an optimal value
+            UNBOUNDED if the solver can found an optimal value but the problem is feasible
+        '''
         raise NotImplementedError("Method solve is not implemented")
