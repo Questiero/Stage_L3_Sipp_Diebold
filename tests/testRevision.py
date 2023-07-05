a6354ff0d474c1992da2c516bfc5dbaf55de411c
Di3bold <erwandiebold54@gmail.com>
2023-07-05 10:27:28 +0200

    Adding rectangle test

--- tests/testRevision.py
+++ tests/testRevision.py
@@ -20,7 +20,7 @@ class TestRevision(unittest.TestCase):
 
         solver = ScipySolverRounded()
         simplifier = [Daalmans(solver)]
-        projector = FloatConvexHullProjector(simplification=simplifier, rounding=12)
+        projector = FloatConvexHullProjector(simplifiers=simplifier, rounding=12)
 
         psi = LinearConstraint("x >= 0") & LinearConstraint("y >= 0") & LinearConstraint("x + y <= 4")
         mu = (LinearConstraint("x + y >= 6") & LinearConstraint("5*x + y <= 25") & LinearConstraint("-0.5*x + y <= 3") & LinearConstraint("1/3*x + y >= 4"))\
@@ -42,7 +42,7 @@ class TestRevision(unittest.TestCase):
 
         solver = ScipySolverRounded()
         simplifier = [Daalmans(solver)]
-        projector = FloatConvexHullProjector(simplification=simplifier, rounding=10)
+        projector = FloatConvexHullProjector(simplifiers=simplifier, rounding=10)
 
         cd = LinearConstraint("vol_tequila >= 0") & LinearConstraint("0.35*vol_tequila - vol_alcool = 0")\
             & LinearConstraint("0.6*vol_sirop + 0.2 * vol_tequila - pouvoirSucrant = 0")
@@ -53,5 +53,28 @@ class TestRevision(unittest.TestCase):
         res = rev.execute(psi, mu)
 
         self.assertTrue(simplifier[0]._interpreter.sat(res[1].toLessOrEqConstraint().toDNF()), "The revision of cocktail simplified is insat.")
+
+    def test_rectangle(self):
+        weights = {
+            RealVariable.declare("x"): Fraction(1),
+            RealVariable.declare("y"): Fraction(1),
+        }
+
+        solver = ScipySolverRounded()
+        simplifier = [Daalmans(solver)]
+        projector = FloatConvexHullProjector(simplifiers=simplifier, rounding=10)
+
+        mu = LinearConstraint("x >= 2") & LinearConstraint("x <= 5") & LinearConstraint("y >= 2") & LinearConstraint("y <= 5")
+        psi = LinearConstraint("x >= 0") & LinearConstraint("x <= 7") & LinearConstraint("y >= 7") & LinearConstraint("y <= 9")\
+        | LinearConstraint("x >= 0") & LinearConstraint("x <= 7") & LinearConstraint("y >= -1") & LinearConstraint("y <= 0")\
+        | LinearConstraint("x >= 7") & LinearConstraint("x <= 9") & LinearConstraint("y >= 0") & LinearConstraint("y <= 7")\
+        | LinearConstraint("x >= -2") & LinearConstraint("x <= 0") & LinearConstraint("y >= 0") & LinearConstraint("y <= 7")
+
+        rev = Revision(solver, discreteL1DistanceFunction(weights), simplifier, onlyOneSolution=False, projector=projector)
+        res = rev.execute(psi, mu)
+
+        self.assertEqual(res[1], (LinearConstraint("x = 5") & LinearConstraint("y <= 5") & LinearConstraint("- y <= -2")) | (LinearConstraint("x <= 5") & LinearConstraint("- x <= -2") & LinearConstraint("y = 2")) | (LinearConstraint("x <= 5") & LinearConstraint("- x <= -2") & LinearConstraint("y = 5")) | (LinearConstraint("x = 2") & LinearConstraint("- y <= -2") & LinearConstraint("y <= 5")))
+
+        self.assertTrue(simplifier[0]._interpreter.sat(res[1].toLessOrEqConstraint().toDNF()), "The revision of two rectangle is insat.")
     
 if __name__ == '__main__': unittest.main()
