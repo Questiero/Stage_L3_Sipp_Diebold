9fa51127af3bd1c1edb6054c30a20092f79fd785
Questiero <19290903+Questiero@users.noreply.github.com>
2024-03-12 21:57:48 +0100

    Weights comments (still TODO explain weights)

--- example1.py
+++ example1.py
@@ -13,42 +13,75 @@ from fractions import Fraction
 
 weights = {
     
+    # Presence of almond milk, a boolean variable with a light weight
     PropositionalVariable("almondMilk"): Fraction("1e-6"),
+    # Presence of banana, a boolean variable with a TODO
     PropositionalVariable("banana"): Fraction(1),
+    # Presence of bitterness, a boolean variable with a heavy weight
     PropositionalVariable("bitter"): Fraction("1e6"),
+    # Presence of cow milk, a boolean variable with a light weight
     PropositionalVariable("cowMilk"): Fraction("1e-6"),
+    # Is the recipe for a dessert, a boolean variable with a heavy weight
     PropositionalVariable("dessert"): Fraction("1e6"),
+    # Presence of fruit, a boolean variable with a heavy weight
     PropositionalVariable("fruit"): Fraction("1e6"),
+    # Presence of granulated sugar, a boolean variable with a heavy weight
     PropositionalVariable("granulatedSugar"): Fraction("1e6"),
+    # Presence of ice cubes, a boolean variable with a heavy weight
     PropositionalVariable("iceCube"): Fraction("1e6"),
+    # Presence of kiwi, a boolean variable with a TODO
     PropositionalVariable("kiwi"): Fraction(1),
+    # Presence of milk, a boolean variable with a heavy weight
     PropositionalVariable("milk"): Fraction("1e6"),
+    # Is the recipe for a milkshake, a boolean variable with a heavy weight
     PropositionalVariable("milkshake"): Fraction("1e6"),
+    # Presence of soy milk, a boolean variable with a light weight
     PropositionalVariable("soyMilk"): Fraction("1e-6"),
+    # Presence of vanilla sugar, a boolean variable with a heavy weight
     PropositionalVariable("vanillaSugar"): Fraction("1e6"),
 
+    # Mass (in grams) of almond milk, a nonnegative real (hence lowerBound to 0) with a TODO
     RealVariable.declare("almondMilk_g", lowerBound = Fraction(0)): Fraction(1),
+    # Liters of almond milk, a nonnegative real (hence lowerBound to 0) with a TODO
     RealVariable.declare("almondMilk_L", lowerBound = Fraction(0)): Fraction(1030),
+    # Mass (in grams) of banana, a nonnegative real (hence lowerBound to 0) with a TODO
     RealVariable.declare("banana_g", lowerBound = Fraction(0)): Fraction(1),
+    # Mass (in grams) of cow milk, a nonnegative real (hence lowerBound to 0) with a TODO
     RealVariable.declare("cowMilk_g", lowerBound = Fraction(0)): Fraction(1),
+    # Liters of cow milk, a nonnegative real (hence lowerBound to 0) with a TODO
     RealVariable.declare("cowMilk_L", lowerBound = Fraction(0)): Fraction(1030),
+    # Mass (in grams) of food, a nonnegative real (hence lowerBound to 0) with a heavy weight
     RealVariable.declare("food_g", lowerBound = Fraction(0)): Fraction("1e6"),
+    # Mass (in grams) of fruit, a nonnegative real (hence lowerBound to 0) with a heavy weight
     RealVariable.declare("fruit_g", lowerBound = Fraction(0)): Fraction("1e6"),
+    # Mass (in grams) of granulated sugar, a nonnegative real (hence lowerBound to 0) with a light weight
     RealVariable.declare("granulatedSugar_g", lowerBound = Fraction(0)): Fraction("1e-6"),
+    # Mass (in grams) of ice cube, a nonnegative real (hence lowerBound to 0) with a heavy weight
     RealVariable.declare("iceCube_g", lowerBound = Fraction(0)): Fraction("1e6"),
+    # Mass (in grams) of kiwi, a nonnegative real (hence lowerBound to 0) with a TODO
     RealVariable.declare("kiwi_g", lowerBound = Fraction(0)): Fraction(1),
+    # Mass (in grams) of milk, a nonnegative real (hence lowerBound to 0) with a heavy weight
     RealVariable.declare("milk_g", lowerBound = Fraction(0)): Fraction("1e6"),
+    # Mass (in grams) of soy milk, a nonnegative real (hence lowerBound to 0) with a TODO
     RealVariable.declare("soyMilk_g", lowerBound = Fraction(0)): Fraction(1),
+    # Liters of soy milk, a nonnegative real (hence lowerBound to 0) with a TODO
     RealVariable.declare("soyMilk_L", lowerBound = Fraction(0)): Fraction(1030),
+    # Sweetening power (in grams) of the recipe in grams, a nonnegative real (hence lowerBound to 0) with a heavy weight
     RealVariable.declare("sweeteningPower_g", lowerBound = Fraction(0)): Fraction("1e6"),
+    # Mass (in grams) of vanilla sugar, a nonnegative real (hence lowerBound to 0) with an heavy weight
     RealVariable.declare("vanillaSugar_g", lowerBound = Fraction(0)): Fraction("1e6"),
 
+    # Number of bananas, a nonnegative integer (hence lowerBound to 0) with a TODO
     IntegerVariable.declare("banana_u", lowerBound = Fraction(0)): Fraction(50),
+    # Number of teaspoons of granulated sugar, a nonnegative integer (hence lowerBound to 0) with a TODO
     IntegerVariable.declare("granulatedSugar_tsp", lowerBound = Fraction(0)): Fraction(50),
+    # Number of ice cubes, a nonnegative integer (hence lowerBound to 0) with a TODO
     IntegerVariable.declare("iceCube_u", lowerBound = Fraction(0)): Fraction(50),
+    # Number of kiwis, a nonnegative integer (hence lowerBound to 0) with a TODO
     IntegerVariable.declare("kiwi_u", lowerBound = Fraction(0)): Fraction(50),
     # Number of fruit types, a nonnegative integer (hence lowerBound to 0) with a heavy weight
     IntegerVariable.declare("nb_fruitTypes", lowerBound = Fraction(0)): Fraction("1e6"),
+    # Number of bags of vanilla sugar, a nonnegative integer (hence lowerBound to 0) with a TODO
     IntegerVariable.declare("vanillaSugar_u", lowerBound = Fraction(0)): Fraction(50),
 
 }
