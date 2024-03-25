51c70b18ea6deab0c53a2853a8b8da889712849b
Questiero <19290903+Questiero@users.noreply.github.com>
2024-03-26 00:02:52 +0100

    expected results

--- examplesICCBR2024/example4.CarrotCabbageSalad.py
+++ examplesICCBR2024/example4.CarrotCabbageSalad.py
@@ -116,26 +116,25 @@ res = adaptator.execute(x_src, y_trgt, dk & ak)[1]
 """
      EXPECTED RESULT
 
-00m37.095s | Solution found with distance of 9743:
+00m09.522s | Solution found with distance of 81:
 
-        almondMilk_g = 977.500000000004
-        almondMilk_L = 0.949029126214
-            banana_g = 120.0
-            banana_u = 1
-           cowMilk_g = 0.0
-           cowMilk_L = 0.0
-              food_g = 1445.0
-             fruit_g = 220.0
-   granulatedSugar_g = 14.999999999996
-granulatedSugar_tbsp = 1
-           iceCube_g = 150.0
-           iceCube_u = 6
-              kiwi_g = 100.0
-              kiwi_u = 1
-              milk_g = 977.500000000004
-           soyMilk_g = 0.0
-           soyMilk_L = 0.0
-   sweeteningPower_g = 162.899999999996
-      vanillaSugar_g = 82.5
-      vanillaSugar_u = 11
+             ak1 = 20.0
+             ak2 = 0.0
+        carrot_g = 244.0
+        carrot_u = 4
+          food_g = 752.0
+greenCabbage_cup = 4
+  greenCabbage_g = 376.0
+    lemonJuice_g = 10.0
+    lemonJuice_L = 0.009090909091
+      oliveOil_g = 50.0
+      oliveOil_L = 0.054722556638
+       shallot_g = 30.0
+       shallot_u = 1
+      soySauce_g = 32.0
+      soySauce_L = 0.029712163417
+       vinegar_g = 0.0
+       vinegar_L = 0.0
+         water_g = 10.0
+         water_L = 0.01
 """
